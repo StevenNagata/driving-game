1d09e31093c6e2f8facbf5c8953697324d6c4efb
StevenNagata <nagata.steven1@gmail.com>
2018-09-13 11:10:08 -0700

    Chose a top-down image of a car and created a variable that holds an img element and added it to the page #1

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ class Car {
     this.location = location
   }
 }
+
+var carImage = document.createElement('img')
+carImage.setAttribute('src', 'https://banner2.kisspng.com/20180320/quq/kisspng-car-mercedes-benz-computer-icons-white-modern-car-top-view-5ab18d1e5c5254.5833239915215854383782.jpg')
+document.body.appendChild(carImage)
