259ff519988939a974c1f9f02b2dca1d35aa9bcf
StevenNagata <nagata.steven1@gmail.com>
2018-09-13 11:12:17 -0700

    Created a new car object and passing it the created image and the other parameters #1

--- main.js
+++ main.js
@@ -10,3 +10,5 @@ class Car {
 var carImage = document.createElement('img')
 carImage.setAttribute('src', 'https://banner2.kisspng.com/20180320/quq/kisspng-car-mercedes-benz-computer-icons-white-modern-car-top-view-5ab18d1e5c5254.5833239915215854383782.jpg')
 document.body.appendChild(carImage)
+
+var myCar = new Car(carImage, 10, 'north', [0,0])
